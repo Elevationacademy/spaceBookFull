f5873072511e22a673e36d832f075fa39ca729a5
Hilla Meir <hillameir85@gmail.com>
2018-08-06 11:24:17 +0300

    add functions to main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+//INDIVIDUAL PROJECT EXERCISE 1
+const posts = [];
+let pNumerator = 0;
+
+$('button').on('click', function() {
+    let relevantInput = $(this).closest("div").find("input").val();
+    addPost(pNumerator, relevantInput);
+    render();
+    pNumerator++;
+})
+
+function addPost(id, text) {
+    posts.push({ id: id, text: text });
+}
+
+//INDIVIDUAL PROJECT EXERCISE 2
+function render() {
+    $('.post').remove();
+    for (let post of posts) {
+        let a = `<p class="post" data-id="${post.id}"> <button type="button" class="remove">REMOVE</button> ${post.text}</p>`;
+        $(".posts").append(a);
+    }
+    //add click event to button 
+    $('.post').on('click', '.remove', function() {
+        let pid = $(this).closest('p').attr("data-id");
+        bindEvent(pid);
+    });
+
+}
+
+//INDIVIDUAL PROJECT EXERCISE 3
+function removePost(id) {
+    for (let i in posts) {
+        if (parseInt(id) === parseInt(posts[i].id)) {
+            posts.splice(i, 1);
+            return;
+        }
+    }
+
+}
+
+var bindEvent = function(id) {
+    removePost(id);
+    render();
+}
